85d842bcee05a01730d0533ff5786c5a32c2a7e6
YoLoN <shadyolown13@onet.pl>
2021-11-22 22:51:01 +0100

    Add Fjord norway section

--- assets/build/main.js
+++ assets/build/main.js
@@ -1,1 +1,42 @@
-"use strict";(self.webpackChunkhiking_in_norway=self.webpackChunkhiking_in_norway||[]).push([[179],{264:function(){new function n(){!function(n,i){if(!(n instanceof i))throw new TypeError("Cannot call a class as a function")}(this,n)}}},function(n){var i;i=264,n(n.s=i)}]);
+"use strict";
+(self["webpackChunkhiking_in_norway"] = self["webpackChunkhiking_in_norway"] || []).push([["main"],{
+
+/***/ "./src/scripts/Application.js":
+/*!************************************!*\
+  !*** ./src/scripts/Application.js ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Application; }
+/* harmony export */ });
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Application = function Application() {
+  _classCallCheck(this, Application);
+};
+
+
+
+/***/ }),
+
+/***/ "./src/scripts/main.js":
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Application__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Application */ "./src/scripts/Application.js");
+
+new _Application__WEBPACK_IMPORTED_MODULE_0__["default"]();
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/scripts/main.js"));
+/******/ }
+]);
